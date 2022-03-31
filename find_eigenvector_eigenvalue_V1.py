6636ce0189defaf40b4d1e293fe34af0a87e10f9
François L. GIRARD <82939484+francoisgirard51@users.noreply.github.com>
2022-03-31 17:13:59 +0200

    Add files via upload

---
+++ find_eigenvector_eigenvalue_V1.py
@@ -0,0 +1,13 @@
+#
+
+A = np.array([[1, 2],
+              [3, 4]])
+V1 = np.array([[5],
+              [6]])
+
+for n in range (10):
+    c1 = np.linalg.norm(V1)
+    W1 = ((1/c1)*V1)
+    V1 = np.dot(A, W1)
+
+    print('\n- ', c1, W1)
---
+++ find_eigenvector_eigenvalue_V2.py
@@ -0,0 +1,19 @@
+#
+
+A = np.array([[1, 2],
+              [3, 4]])
+I = np.identity(2)
+
+B = (np.dot(c1, I) - A)
+
+V2 = np.array([[10],
+              [5]])
+
+print('Matrix B =',B)
+
+for n in range (10):
+    c2 = np.linalg.norm(V2)
+    W2 = ((1/c2)*V2)
+    V2 = np.dot(B, W2)
+
+    print('\n- ', c2, W2)
