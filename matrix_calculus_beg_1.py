cec6aab42607bf3b53ef5835abf59982f278168d
François L. GIRARD <82939484+francoisgirard51@users.noreply.github.com>
2022-03-31 17:26:57 +0200

    Add files via upload

---
+++ matrix_calculus_beg_1.py
@@ -0,0 +1,14 @@
+# Matrix calculus: Beginner level 1
+
+A = np.array([[1, 2, 3],
+              [4, 5, 6]])
+
+X = np.array([[7],
+              [8],
+              [9]])
+
+Y = np.dot(A,X)
+print("Matrix product 𝑌 = 𝐴𝑋:\n", Y)
+print("\nMatrix A:\n", A)
+print("\nVector Y:\n", Y)
+print("\nVector X:\n", X)
